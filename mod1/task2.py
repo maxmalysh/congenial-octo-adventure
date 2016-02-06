72784da5b663fd6077808e736d79dc0c0f2eec6f
maxmalysh <maxmalysh@gmail.com>
2016-02-06 05:25:42 +0300

    Added an option to calculate residual vector with double precision

--- mod1/task2.py
+++ mod1/task2.py
@@ -26,8 +26,8 @@ test_vector = np.array([
 
 
 def plup_solve(A: np.matrix, b: np.array) -> np.array:
-    A = A.astype(np.float)
-    b = b.astype(np.float)
+    A = A.astype(np.float32)
+    b = b.astype(np.float32)
 
     P,L,U,P_ = PLUP_decomposition(A)
     Pb = P.transpose().dot(b)
@@ -53,13 +53,14 @@ def plup_solve(A: np.matrix, b: np.array) -> np.array:
 
 # Итерационное уточнение - стр. 34, два пункта сверху
 # Iterative refinement
-def plup_solve_iterative(A: np.matrix, b: np.array) -> np.array:
+def plup_solve_iterative(A: np.matrix, b: np.array, double_precision=False) -> np.array:
     x = plup_solve(A, b)
 
     iteration_count = 2
 
     for i in range(0, iteration_count):
-        r = b - A.dot(x)
+        chosen_type = np.longfloat if double_precision else np.float
+        r = b.astype(chosen_type) - A.astype(chosen_type).dot(x).astype(chosen_type)
         z = plup_solve(A, r)
         x = x + z
 
@@ -101,9 +102,10 @@ if __name__ == '__main__':
 
     numpyResult = npsolve(matrix.astype(np.float64), vector.astype(np.float64))
     ourResult = plup_solve(matrix, vector)
-    ourBetterResult = plup_solve_iterative(matrix, vector)
+    ourBetterResult = plup_solve_iterative(matrix, vector, double_precision=False)
+    ourEvenBetterResult = plup_solve_iterative(matrix, vector, double_precision=True)
 
-    print(solution_deviation(matrix, numpyResult, vector))
-    print(solution_deviation(matrix, ourResult, vector))
-    print(solution_deviation(matrix, ourBetterResult, vector))
+    results = [numpyResult, ourResult, ourBetterResult, ourEvenBetterResult]
+    for result in results:
+        print(solution_deviation(matrix, result, vector))
 
