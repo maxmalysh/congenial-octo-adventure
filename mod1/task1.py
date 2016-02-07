018866d4845da481eeaa7d714a9e220e3c321168
Andrey Moiseev <o2g.org.ru@gmail.com>
2016-02-08 00:37:04 +0300

    Sparse matrix decomposition

--- mod1/task1.py
+++ mod1/task1.py
@@ -1,12 +1,15 @@
 import numpy as np
 from typing import List
 from enum import Enum
+from scipy import sparse
+from random import randint
 
 
 class PivotMode(Enum):
     BY_ROW = 0
     BY_COLUMN = 1
     BY_MATRIX = 2
+    SPARSE = 3
 
 
 def pivot_by_row(A: np.matrix, k: int, row_permutation: List[float]):
@@ -61,6 +64,98 @@ def pivot_by_matrix(A: np.matrix, k: int, row_permutation: List[float], column_p
         column_permutation[column_with_max_leading_elem] = column_perm_k_saved
 
 
+def reverse_permutation(perm: List[int]):
+    rev_perm = [0] * len(perm)
+    for i in range(len(perm)):
+        rev_perm[perm[i]] = i
+    return rev_perm
+
+
+def pivot_sparse(A: sparse.dok_matrix, k: int, row_permutation: List[float], column_permutation: List[float]):
+    n = A.shape[0]
+
+    # Using Tewarson theorem (LecV.pdf, p. 35)
+
+    # Usual dense matrices. B_ is not sparse anyway, and we'll need use it in multiplication.
+    B = np.zeros((n - k, n - k),
+                 dtype=np.integer)  # Take gaussian step submatrix of A, put 1s in place of non-zero elements.
+    B_ = np.zeros((n - k, n - k),
+                  dtype=np.integer)  # Take gaussian step submatrix of A, put 0s in place of non-zero elements,
+    # 1s in place of zero elements.
+
+    # To use in iteration over non-zero A submatrix elements.
+    row_permutation_reverse = reverse_permutation(row_permutation)
+    column_permutation_reverse = reverse_permutation(column_permutation)
+
+    # Calculate B and B_
+    # An optimization. We only iterate over non-zero A elements (and skip those out of bounds of gaussian submatrix).
+    for i_permuted, j_permuted in A.keys():  # Warning! These indices are already permuted. We have to unpermute them.
+        # Find unpermuted indices, subtract k to convert to B or B_ indices. Corresponding A indices are i+k, j+k.
+        i = row_permutation_reverse[i_permuted] - k
+        j = column_permutation_reverse[j_permuted] - k
+        # Skip elements that are out of submatrix
+        if i < 0 or j < 0:
+            continue
+        # if A[row_permutation[i+k], column_permutation[j+k]] != 0:
+        if A[i_permuted, j_permuted] != 0:
+            B[i, j] = 1
+        else:
+            B_[i, j] = 1
+
+    G = B @ B_.transpose() @ B
+
+    # Threshold to decide whether an element is almost-zero.
+    epsilon = -1  # 1e-08
+    # If abs(elem) < epsilon, we will not accept such an element as leading element.
+
+    # Now find leading element.
+    # 1. Among non-almost-zero elements of A submatrix, find those with minimal corresponding G elements.
+
+    g_min_elem_indices = []  # Indices of minimal elements of G.
+
+    # An optimization. We only iterate over non-zero A elements (and skip those out of bounds of gaussian submatrix).
+    for i_permuted, j_permuted in A.keys():  # Warning! These indices are already permuted. We have to unpermute them.
+        # Find unpermuted indices, subtract k to convert to G indices. Corresponding A indices are i+k, j+k.
+        i = row_permutation_reverse[i_permuted] - k
+        j = column_permutation_reverse[j_permuted] - k
+        # Skip elements that are out of submatrix
+        if i < 0 or j < 0:
+            continue
+        # Skip almost-zero element
+        # if abs(A[row_permutation[i + k], column_permutation[j + k]]) < epsilon:
+        if abs(A[i_permuted, j_permuted]) < epsilon:
+            continue
+        if len(g_min_elem_indices) == 0:  # Found first non-almost-zero element. Use is as current minimum.
+            g_min_elem_indices.append((i, j))
+        if G[i, j] < G[g_min_elem_indices[0]]:  # Found the element that is smaller than current minimum
+            g_min_elem_indices.clear()
+            g_min_elem_indices.append((i, j))
+        elif G[i, j] == G[g_min_elem_indices[0]]:  # Found one more current minimum element
+            g_min_elem_indices.append((i, j))
+
+    if len(g_min_elem_indices) == 0:
+        raise ValueError("Unable to find non-almost-zero elements in A submatrix")
+
+    # 2. If more than one element found, choose the one with maximal corresponding A submatrix element (absolute value).
+    g_chosen_elem_index = g_min_elem_indices[0]
+    for g_elem_index in g_min_elem_indices:
+        if abs(A[row_permutation[g_elem_index[0] + k], column_permutation[g_elem_index[1] + k]]) \
+                > abs(A[row_permutation[g_chosen_elem_index[0] + k], column_permutation[g_chosen_elem_index[1] + k]]):
+            g_chosen_elem_index = g_elem_index
+
+    # Now swap rows and columns to make the chosen element leading.
+
+    row_with_chosen_leading_elem = g_chosen_elem_index[0] + k
+    column_with_chosen_leading_elem = g_chosen_elem_index[1] + k
+
+    row_perm_k_saved = row_permutation[k]
+    row_permutation[k] = row_permutation[row_with_chosen_leading_elem]
+    row_permutation[row_with_chosen_leading_elem] = row_perm_k_saved
+
+    column_perm_k_saved = column_permutation[k]
+    column_permutation[k] = column_permutation[column_with_chosen_leading_elem]
+    column_permutation[column_with_chosen_leading_elem] = column_perm_k_saved
+
 def pivot(A: np.matrix, k: int, mode: PivotMode, row_permutation: List[float], column_permutation: List[float]):
     if mode == PivotMode.BY_ROW:
         pivot_by_row(A, k, row_permutation)
@@ -68,6 +163,8 @@ def pivot(A: np.matrix, k: int, mode: PivotMode, row_permutation: List[float], c
         pivot_by_column(A, k, column_permutation)
     elif mode == PivotMode.BY_MATRIX:
         pivot_by_matrix(A, k, row_permutation, column_permutation)
+    elif mode == PivotMode.SPARSE:
+        pivot_sparse(A, k, row_permutation, column_permutation)
 
 
 '''
@@ -122,9 +219,8 @@ P is transposition matrix.
 # Not an in-place algorithm.
 '''
 
-
 def lpu_decompose(A: np.matrix) -> (np.matrix, np.matrix, np.matrix):
-    A = A.astype(np.float)
+    A = A.copy()
     n = A.shape[0]
     L = np.identity(A.shape[0])
     U = np.identity(A.shape[0])
@@ -187,7 +283,6 @@ P is transposition matrix.
 
 
 def lpl_decompose(A: np.matrix) -> (np.matrix, np.matrix, np.matrix):
-    A = A.astype(np.float)
     # LecV.pdf p. 27
     Q = np.identity(A.shape[0])
     Q = Q[::-1]
@@ -222,24 +317,6 @@ def lu_extract(LU: np.matrix, row_permutation: List[float], column_permutation: 
     return L, U
 
 
-def demo():
-    from tests import test_matrices
-    A = test_matrices[4]
-
-    print(A)
-    print()
-
-    lu_in_one, P, P_ = lu_decompose_pivoting(A.copy(), PivotMode.BY_MATRIX)
-    L, U = lu_extract(lu_in_one, P, P_)
-    print(P)
-    print(P_)
-    print(L @ U)
-
-
-if __name__ == "__main__":
-    demo()
-
-
 '''
 @vector - permutation vector as a list, e.g. [2,0,1]
 @row – boolean determining whether vector is shuffling rows or columns
@@ -256,6 +333,27 @@ def perm_vector_to_matrix(vector, row=True):
 
     return matrix
 
+
+def demo():
+    from tests import test_matrices
+    A = sparse.dok_matrix(test_matrices[7].astype(np.float))
+
+    print(A.todense())
+    print()
+
+    lu_in_one, P, P_ = lu_decompose_pivoting(A, PivotMode.SPARSE)
+    L, U = lu_extract(lu_in_one, P, P_)
+    print(P)
+    print(P_)
+    np.set_printoptions(precision=2)
+    print(lu_in_one)
+    print(perm_vector_to_matrix(P, row=True) @ L @ U @ perm_vector_to_matrix(P_, row=False))
+
+
+if __name__ == "__main__":
+    demo()
+
+
 def PLU_decomposition(A):
     mode = PivotMode.BY_ROW
     lu_in_one, P, P_ = lu_decompose_pivoting(A.astype(np.float), mode)
@@ -271,6 +369,13 @@ def LUP_decomposition(A):
 def PLUP_decomposition(A):
     mode = PivotMode.BY_MATRIX
     lu_in_one, P, P_ = lu_decompose_pivoting(A.astype(np.float), mode)
+    L, U = lu_extract(lu_in_one, P, P_)
+    return perm_vector_to_matrix(P, row=True), L, U, perm_vector_to_matrix(P_, row=False)
+
+
+def Sparse_decomposition(A):
+    mode = PivotMode.SPARSE
+    lu_in_one, P, P_ = lu_decompose_pivoting(A, mode)
     L, U = lu_extract(lu_in_one, P, P_)
     return perm_vector_to_matrix(P, row=True), L, U, perm_vector_to_matrix(P_, row=False)
 
--- mod1/tests.py
+++ mod1/tests.py
@@ -1,7 +1,8 @@
 import unittest
 import numpy as np
 import random
-from task1 import PLU_decomposition, LUP_decomposition, PLUP_decomposition
+from scipy import sparse
+from task1 import PLU_decomposition, LUP_decomposition, PLUP_decomposition, Sparse_decomposition
 from task1 import lpl_decompose, lpu_decompose
 
 np.seterr(all='raise') # 'raise' / 'print' / 'ignore'
@@ -55,6 +56,14 @@ test_matrices = [
         [0, 8, 2, 6, 3],
         [0, 3, 2, 2, 2],
     ]),
+
+    np.matrix([
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+    ])
 ]
 
 
@@ -103,6 +112,14 @@ class LPL_Test(DecompositionTest):
         self.assertTrue(equal)
 
 
+class Sparse_Test(DecompositionTest):
+    def runTest(self):
+        P, L, U, P_ = Sparse_decomposition(self.matrix)
+        result = P @ L @ U @ P_
+        equal = np.allclose(self.matrix.todense(), result)
+        self.assertTrue(equal)
+
+
 def get_random_suite():
     suite = unittest.TestSuite()
     test_types = [
@@ -113,6 +130,7 @@ def get_random_suite():
 
     for i in range(0, 20):
         matrix = np.random.randint(low=-100, high=100, size=(30,30))
+        matrix = matrix.astype(np.float)
         tests = [ test(matrix, i) for test in test_types ]
         suite.addTests(tests)
 
@@ -131,10 +149,31 @@ def get_random_nonsingular_suite():
 
         while singular:
             matrix = np.random.randint(low=-100, high=100, size=(50,50))
+            matrix = matrix.astype(np.float)
             if np.linalg.det(matrix) != 0:
                 singular = False
 
         tests = [test(matrix, i) for test in test_types]
+        suite.addTests(tests)
+
+    return suite
+
+
+def get_random_sparse_nonsingular_suite():
+    suite = unittest.TestSuite()
+
+    for i in range(0, 10):
+        singular = True
+
+        while singular:
+            n = 50
+            matrix = sparse.dok_matrix((n, n), dtype=np.float)
+            for rand_iterations in range(0, int((n ** 2) / 4)):
+                matrix[random.randint(0, n - 1), random.randint(0, n - 1)] = random.randint(-100, 100)
+            if np.linalg.det(matrix.todense()) != 0:
+                singular = False
+
+        tests = [Sparse_Test(matrix, i)]  # PLUP_Test(matrix.todense(), i),
         suite.addTests(tests)
 
     return suite
@@ -157,5 +196,6 @@ def get_suite():
 
 
 if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(get_random_suite())
-    unittest.TextTestRunner(verbosity=2).run(get_random_nonsingular_suite())
+    # unittest.TextTestRunner(verbosity=2).run(get_random_suite())
+    # unittest.TextTestRunner(verbosity=2).run(get_random_nonsingular_suite())
+    unittest.TextTestRunner(verbosity=2).run(get_random_sparse_nonsingular_suite())
