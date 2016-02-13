de6284bb0c10c0597d231eb0959fcafb08183560
Andrey Moiseev <o2g.org.ru@gmail.com>
2016-02-13 15:02:20 +0300

    Preconditioned conjugate gradients method

--- mod2/task1.py
+++ mod2/task1.py
@@ -34,59 +34,53 @@ import numpy as np
 import math
 from typing import Tuple, Set
 
-
-# Golub, van Loun 4rd ed., p. 632
-def conj_gradients_method(A: np.matrix, b: np.ndarray, x_0: np.ndarray):
-    x = {};
-    r = {};
-    beta = {};
-    alpha = {};
-    q = {};
-    c = {};
-    d = {};
-    nu = {};
-    l = {};
-
-    x[0] = x_0
-    r[0] = b - A @ x_0
-    beta[0] = np.linalg.norm(r[0], ord=2)
-    q[0] = 0
-    c[0] = 0
-
+def conj_grad_precond(A: np.matrix, b: np.ndarray, x_0: np.ndarray, precond_func):
     k = 0
-    while (not math.isclose(beta[k], 0)) and (k < A.shape[0]):
-        q[k + 1] = r[k] / beta[k]
+    r = {}; r[0] = A @ x_0 - b
+    x = {}; x[0] = x_0
+    z = {}
+    p = {}
+
+    L, U = precond_func(A)
+    M = L @ U
+    z[0] = np.linalg.solve(M, r[0])
+
+    while not math.isclose(np.linalg.norm(r[k], ord=2), 0):
         k += 1
-        alpha[k] = q[k].transpose() @ A @ q[k]
-
         if k == 1:
-            print(alpha[k])
-            d[1] = alpha[1]
-            nu[1] = beta[0] / d[1]
-            c[k] = q[1]
+            p[k] = z[0]
         else:
-            l[k - 1] = beta[k - 1] / d[k - 1]
-            d[k] = alpha[k] - beta[k - 1] * l[k - 1]
-            nu[k] = -beta[k - 1] * nu[k - 1] / d[k]
-            c[k] = q[k] - l[k - 1] * c[k - 1]
-
-        x[k] = x[k - 1] + nu[k] * c[k]
-        r[k] = A @ q[k] - alpha[k] * q[k] - beta[k - 1] * q[k - 1]
-        beta[k] = np.linalg.norm(r[k], ord=2)
+            tau = (r[k-1].transpose() @ z[k-1]) / (r[k-2].transpose() @ z[k-2])
+            p[k] = z[k-1] + tau * p[k-1]
+        mu = (r[k-1].transpose() @ z[k-1]) / (p[k].transpose() @ A @ p[k])
+        x[k] = x[k-1] - mu * p[k]
+        r[k] = r[k-1] - mu * (A @ p[k])
+        z[k] = np.linalg.solve(M, r[k])
 
     x_star = x[k]
-    return x_star
+    return x_star, k
 
 
-def matrix_portrait(A: np.matrix) -> Set[Tuple[int, int]]:
-    Omega = set()
-    n = A.shape[0]
-    for i in range(0, n):
-        for j in range(0, n):
-            if not math.isclose(A[i, j], 0):
-                Omega.add((i, j))
 
-    return Omega
+def matrix_portrait(A: np.matrix, e: float = None) -> Set[Tuple[int, int]]:
+    if e is None:
+        Omega = set()
+        n = A.shape[0]
+        for i in range(0, n):
+            for j in range(0, n):
+                if not math.isclose(A[i, j], 0):
+                    Omega.add((i, j))
+
+        return Omega
+    else:
+        Omega = set()
+        n = A.shape[0]
+        for i in range(0, n):
+            for j in range(0, n):
+                if abs(A[i, j]) > e or i==j:
+                    Omega.add((i, j))
+
+        return Omega
 
 
 def s_compute(R: np.matrix, i: int):
@@ -118,38 +112,32 @@ def incomplete_lu(A: np.matrix, Omega: Set[Tuple[int, int]], modified: bool) -> 
     return L, R
 
 
-def ilu_k(A: np.matrix, k: int, modified: bool) -> Tuple[np.matrix, np.matrix]:
-    Omega = matrix_portrait(A)
+def ilu_k(A: np.matrix, k: int, modified: bool = False, e: float = None) -> Tuple[np.matrix, np.matrix]:
+    Omega = matrix_portrait(A, e)
     for i in range(0, k+1):
         L, R = incomplete_lu(A, Omega, modified)
         T = L @ R - A
-        Omega |= matrix_portrait(T) # | is set union
+        Omega |= matrix_portrait(T, e) # | is set union
 
     return L, R
 
 
 if __name__ == "__main__":
-    np.set_printoptions(precision=2)
+    np.set_printoptions(precision=4)
     A = np.array(
-       [[ 6.,  0.,  0., -1., -1.],
-       [ 0.,  8., -4., -2.,  0.],
-       [ 0., -4.,  9.,  0.,  0.],
-       [-1., -2.,  0.,  9.,  0.],
-       [-1.,  0.,  0.,  0.,  7.]], dtype=float)
+       [[ 77., -23., -32.,   0.,   0.],
+       [-23.,  53.,   0.,   0., -18.],
+       [-32.,   0.,  90.,  -5.,   0.],
+       [  0.,   0.,  -5.,  49., -15.],
+       [  0., -18.,   0., -15.,  89.]], dtype=float)
 
-    L, R = ilu_k(A, 0, True)
-    T = L@R - A
-    L2, R2 = ilu_k(A, 0, False)
-    T2 = L2@R2 - A
-    print(L - L2)
-    print(R - R2)
-    print(T - T2)
+    b = np.array([1, 1, 1, 1, 1], dtype=float)
+    x_0 = np.array([0, 0, 0, 0, 0], dtype=float)
 
-    #print(L@R - A)
+    x_star = np.linalg.solve(A.copy(), b)
+    print("NUMPY:")
+    print(x_star)
 
-    #A = np.array([[3.0, -1.0],
-    #              [-1.0, 3.0]])
-    #b = np.array([1.0, 2.0])
-    #x_0 = np.array([1000, 1000])
-    #x_star = conj_gradients_method(A, b, x_0)
-    #print(x_star)
+    x_star, k = conj_grad_precond(A.copy(), b, x_0, lambda A: ilu_k(A, 10))
+    print("PRECOND CONJ GRAD, iterations: " + str(k))
+    print(x_star)
